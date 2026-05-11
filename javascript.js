ef04de8fa42330ea9aacd517f9c48bfe1d14fc96
Norman Lew <normanlew@users.noreply.github.com>
2026-05-11 15:35:33 -0700

    Add functionality to createGameControl and test
    Added functionality to the createGameControl class to play the
    entire game from start to finish.

--- javascript.js
+++ javascript.js
@@ -19,15 +19,14 @@ const gameBoard = (() => {
 
     const placeSymbol = (symbol, space) => {
         symbol = symbol.toUpperCase();
-        if (boardIsFull || gameIsWon || space < 0 || space > 8 || board[space] !== undefined || size > 9) {
+        if (boardIsFull || gameIsWon || space < 0 || space > 8 || board[space] !== undefined || size >= 9) {
             return false;
         }
 
         else {
             board[space] = symbol;
             size++;
-            console.log(size);
-            if (size === 9) {
+            if (size >= 9) {
                 boardIsFull = true;
             }
 
@@ -54,15 +53,54 @@ const gameBoard = (() => {
 })();
 
 function createPlayer(name, symbol) {
+    symbol = symbol.toUpperCase();
     const getName = () => name;
     const getSymbol = () => symbol;
 
     return {getName, getSymbol};
 }
 
-const gameControl = ((player1, player2, gameBoard) => {
-    player1Turn = true;
+function createGameControl(player1, player2, gameBoard) {
+    let player1Turn = Math.random() > 0.5;
+    const playGame = (() => {
+        do {
+            let player;
 
-    
+            if (player1Turn) {
+                player = player1;
+            }
+            else {
+                player = player2;
+            }
 
-})();
+            const spaceNumber = prompt(`${player.getName()}, choose a space`);
+            console.log(spaceNumber);
+            if (!gameBoard.placeSymbol(player.getSymbol(), spaceNumber)) {
+                console.log("try another space");
+            } else {
+                if (player1Turn) {
+                    player1Turn = false;
+                }
+                else {
+                    player1Turn = true;
+                }
+            }
+        } while (!gameBoard.getBoardIsFull() && !gameBoard.getGameIsWon());
+        
+        console.log(gameBoard.getBoardIsFull());
+        console.log(gameBoard.getGameIsWon());
+        if (gameBoard.getGameIsWon()) {
+            if (player1.getSymbol() === gameBoard.getWinner()) {
+                console.log(`${player1.getName()} wins!`)
+            }
+            else {
+                console.log(`${player2.getName()} wins!`)
+            }
+        }
+        else if (gameBoard.getBoardIsFull()) {
+            console.log("The board is full and nobody won.")
+        }
+    });
+
+    return {playGame};
+};
