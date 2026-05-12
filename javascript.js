7e48f72e7748aa859685afd7ea511fb61f1b9b6d
Norman Lew <normanlew@users.noreply.github.com>
2026-05-12 13:16:27 -0700

    Create event listener on each space
    Created an event listener on each space.  When clicked, it
    sends the div clicked to the game controller for processing.

--- javascript.js
+++ javascript.js
@@ -61,36 +61,35 @@ function createPlayer(name, symbol) {
 }
 
 function createGameControl(player1, player2, gameBoard) {
+    const dict = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+                    "six": 6, "seven": 7, "eight": 8};
+
     let player1Turn = Math.random() > 0.5;
 
-    const squares = document.querySelectorAll(".square")
-    const playGame = (() => {
-        do {
-            let player;
+    // const squares = document.querySelectorAll(".square")
 
+    const putSymbolOnBoard = ((element) => {
+        let player;
+        let spaceNumber = dict[element.id];
+
+        if (player1Turn) {
+            player = player1;
+        }
+        else {
+            player = player2;
+        }
+
+        if (!gameBoard.placeSymbol(player.getSymbol(), spaceNumber)) {
+            console.log("try another space");
+        } else {
             if (player1Turn) {
-                player = player1;
+                player1Turn = false;
             }
             else {
-                player = player2;
+                player1Turn = true;
             }
+        }
 
-            const spaceNumber = prompt(`${player.getName()}, choose a space`);
-            console.log(spaceNumber);
-            if (!gameBoard.placeSymbol(player.getSymbol(), spaceNumber)) {
-                console.log("try another space");
-            } else {
-                if (player1Turn) {
-                    player1Turn = false;
-                }
-                else {
-                    player1Turn = true;
-                }
-            }
-        } while (!gameBoard.getBoardIsFull() && !gameBoard.getGameIsWon());
-        
-        // console.log(gameBoard.getBoardIsFull());
-        // console.log(gameBoard.getGameIsWon());
         if (gameBoard.getGameIsWon()) {
             if (player1.getSymbol() === gameBoard.getWinner()) {
                 console.log(`${player1.getName()} wins!`)
@@ -103,6 +102,60 @@ function createGameControl(player1, player2, gameBoard) {
             console.log("The board is full and nobody won.")
         }
     });
+    // const playGame = (() => {
+    //     do {
+    //         let player;
 
-    return {playGame};
+    //         if (player1Turn) {
+    //             player = player1;
+    //         }
+    //         else {
+    //             player = player2;
+    //         }
+
+    //         const spaceNumber = prompt(`${player.getName()}, choose a space`);
+    //         console.log(spaceNumber);
+    //         if (!gameBoard.placeSymbol(player.getSymbol(), spaceNumber)) {
+    //             console.log("try another space");
+    //         } else {
+    //             if (player1Turn) {
+    //                 player1Turn = false;
+    //             }
+    //             else {
+    //                 player1Turn = true;
+    //             }
+    //         }
+    //     } while (!gameBoard.getBoardIsFull() && !gameBoard.getGameIsWon());
+        
+    //     // console.log(gameBoard.getBoardIsFull());
+    //     // console.log(gameBoard.getGameIsWon());
+    //     if (gameBoard.getGameIsWon()) {
+    //         if (player1.getSymbol() === gameBoard.getWinner()) {
+    //             console.log(`${player1.getName()} wins!`)
+    //         }
+    //         else {
+    //             console.log(`${player2.getName()} wins!`)
+    //         }
+    //     }
+    //     else if (gameBoard.getBoardIsFull()) {
+    //         console.log("The board is full and nobody won.")
+    //     }
+    // });
+    
+
+    return {putSymbolOnBoard};
+
 };
+
+const mary = createPlayer("Mary", "X");
+const reuben = createPlayer("Reuben", "Y");
+
+const game = createGameControl(mary, reuben, gameBoard);
+
+const squares = document.querySelectorAll(".square")
+
+squares.forEach((element) => {
+    element.addEventListener(("click"), () => {
+        game.putSymbolOnBoard(element);
+    })
+})
